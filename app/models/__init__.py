2e446ad8ccab5daba15ac3b19c7e79212657d91f
Yannaev <69728833+yannaev@users.noreply.github.com>
2026-03-31 14:09:07 +0500

    add user model

---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.user import UserORM
+
+
+__all__ = ["UserORM"]
---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BigInteger, func, DateTime, false, String
+
+from app.database.base import Base
+
+
+class UserORM(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(320), unique=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
